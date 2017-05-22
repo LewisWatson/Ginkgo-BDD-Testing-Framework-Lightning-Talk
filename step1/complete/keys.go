fb8562c04610ec11e48b513683a083a8ef10ede2
Lewis Watson <l.watson@sensewhere.com>
2017-05-22 17:09:56 +0100

    Complete step 1

--- step1/complete/keys.go
+++ step1/complete/keys.go
@@ -1,7 +1,11 @@
 package keys
 
+import (
+	"fmt"
+)
+
 // GetKeys client tokens must be signed by one of the server keys provided via a url.
 // The keys expire after a certain amount of time so we need to track that also.
 func GetKeys(keyURL string) (tokens map[string]interface{}, maxAge int64, err error) {
-	return nil, 0, nil
+	return nil, 0, fmt.Errorf("blah")
 }
